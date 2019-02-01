a7e191e04a0d6bc47274048c963a6bf8f1480790
Alex Garcia <alejandragviana@gmail.com>
2019-02-01 11:22:45 +0100

    persons incoming and requested

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import './styles/App.scss';
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>hola</h1>
+        <h1></h1>
+        <main className="main">
+          <div className="main__whoCalls">
+            <h2 className="main__whocalls--title">¿Quién atendió la llamada</h2>
+            <select className="main__employees"></select>
+          </div>
+
+          <div className="main__requested--employee">
+            <h2 className="main__requested--employee-title">¿Por quién preguntaban?</h2>
+            <select className="main__employees"></select>
+          </div>
+
+          
+        </main>
       </div>
     );
   }
