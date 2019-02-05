bcbacd991157f8f17cd20d770fcaccca565b36e4
lilia <liliaborrazas14@gmail.com>
2019-02-05 13:42:09 +0100

    merge with styles and the finish working in this branch

--- src/App.js
+++ src/App.js
@@ -247,36 +247,37 @@ class App extends Component {
               <fieldset className="form-section incoming-data">
                 <h2 className="incoming-data__title">¿Quién llamó?</h2>
                 <p className={`error-msg ${this.state.errorIncomingData}`}>Debes rellenar al menos uno de los campos</p>
-                <div className="incoming-data__fielsContainer">
+
+                <div className="incoming-data__fieldsContainer">
 
                 <div className="incoming-data__name">
                   <label htmlFor="name" className="incoming-data__name--label label" aria-label="nombre">Nombre</label>
-                  <input id="name" type="text" className="incoming-data__name--input" placeholder="Nombre" onKeyUp={this.getName}/>
+                  <input id="name" type="text" className="incoming-data__name--input incoming-data__input" placeholder="Nombre" onKeyUp={this.getName}/>
                 </div>
               
                 <div className="incoming-data__company">
                   <label htmlFor="company" className="incoming-data__company--label label" aria-label="empresa">Empresa</label>
-                  <input id="company" type="text" className="incoming-data__company--input" placeholder="Empresa" onKeyUp={this.getCompany}/>
+                  <input id="company" type="text" className="incoming-data__company--input incoming-data__input" placeholder="Empresa" onKeyUp={this.getCompany}/>
                 </div>
 
                 <div className="incoming-data__position">
                   <label htmlFor="position" className="incoming-data__position--label label" aria-label="cargo">Cargo</label>
-                  <input id="position" type="text" className="incoming-data__position--input" placeholder="Cargo" onKeyUp={this.getPosition}/>
+                  <input id="position" type="text" className="incoming-data__position--input incoming-data__input" placeholder="Cargo" onKeyUp={this.getPosition}/>
                 </div>
 
                 <div className="incoming-data__other-info">
                   <label htmlFor="other-info" className="incoming-data__other-info--label label" aria-label="otro detalle">Otro detalle</label>
-                  <input id="other-info" type="text" className="incoming-data__other-info--input" placeholder="Otro detalle" onKeyUp={this.getOtherInfo}/>
+                  <input id="other-info" type="text" className="incoming-data__other-info--input incoming-data__input" placeholder="Otro detalle" onKeyUp={this.getOtherInfo}/>
                 </div>
 
                 <div className="incoming-data__email">
                   <label htmlFor="email" className="incoming-data__email--label label" aria-label="email">Email</label>
-                  <input id="email" type="email" className="incoming-data__email--input" placeholder="Email" onKeyUp={this.getEmail}/>
+                  <input id="email" type="email" className="incoming-data__email--input incoming-data__input" placeholder="Email" onKeyUp={this.getEmail}/>
                 </div>
 
                 <div className="incoming-data__mobile">
                   <label htmlFor="mobile" className="incoming-data__mobile--label label" aria-label="teléfono">Teléfono</label>
-                  <input id="mobile" type="tel" className="incoming-data__mobile--input" placeholder="Teléfono" onKeyUp={this.getPhone}/>
+                  <input id="mobile" type="tel" className="incoming-data__mobile--input incoming-data__input" placeholder="Teléfono" onKeyUp={this.getPhone}/>
                 </div>
                 </div>
               
