8a57fcf3df9ee93a60a7c196fdf7420cd0a1516b
lilia <liliaborrazas14@gmail.com>
2019-02-12 11:58:22 +0100

    test branch

--- src/App.js
+++ src/App.js
@@ -254,7 +254,7 @@ getCallAction(event) {
 
     if ((this.state.info.name!==''|| this.state.info.position!=='' || this.state.info.company!=='' || this.state.info.otherInfo!==''|| this.state.info.email!=='' )&& this.state.info.telephone===0){
       return message=  `${this.state.info.personRequested}, *te acaba de llamar*: \n
-      ${this.state.info.name} \n${this.state.info.position} \n${this.state.info.company}  \n${this.state.info.email} \n${this.state.info.otherInfo} \n *Su mensaje ha sido* \n${this.state.info.action} \n${this.state.info.message}`;
+      ${this.state.info.name} \n${this.state.info.position} \n${this.state.info.company}  \n${this.state.info.email} \n${this.state.info.otherInfo} \n*Su mensaje ha sido* ${this.state.info.action} \n${this.state.info.message}`;
     }
     else if (this.state.info.name!==''|| this.state.info.position!=='' || this.state.info.company!=='' || this.state.info.otherInfo!==''|| this.state.info.email!=='' || this.state.info.telephone!==0){
       return message=  `${this.state.info.personRequested}, *te acaba de llamar*: \n
@@ -269,6 +269,8 @@ getCallAction(event) {
   sendSlackInfo(){
     
     const message = this.makeMessage();
+    //Holaa te subes?
+    
     const key = KEYS.SLACK_KEY;
 
     const settings = {
