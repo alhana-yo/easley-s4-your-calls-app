a052566c2e119a3cec6edfd01e883c501a27996b
Alex Garcia <alejandragviana@gmail.com>
2019-02-13 15:11:53 +0100

    date and hour correctly written

--- src/components/CallHistory.js
+++ src/components/CallHistory.js
@@ -9,12 +9,14 @@ class CallHistory extends Component {
         return (
             <Fragment>
                 <ul className="list">
-                {console.log(results)}
                     {results.map(item => {
                         return(
                             <li className="list__item" key={item._id} >
                                 <div className="list__item--container">
-                                  <div className="date"><p className="date">{item.loggedAt.split("T")[0]}</p> <p className="hour">{item.loggedAt.split("T")[1].split("", 8)}</p></div>
+                                  <div className="date">
+                                    <p className="date">{item.loggedAt.split("T")[0].split("-").reverse().join("/")}</p> 
+                                    <p className="hour">{item.loggedAt.split("T")[1].split("", 5)}h</p>
+                                  </div>
                                   <p className="askfor">{item.personRequested}</p>
                                   <p className="name">{item.name}</p>
                                   <p className="company">{item.company}</p>
