a301c718b5d59f9f46231ee4a6b95d3e481b8f9f
Alex Garcia <alejandragviana@gmail.com>
2019-02-13 12:11:52 +0100

    date and hour component list

--- src/components/CallHistory.js
+++ src/components/CallHistory.js
@@ -4,18 +4,20 @@ class CallHistory extends Component {
     componentDidMount(){
         this.props.actionShowList();
     }
-
     render() {
-       const {results} = this.props;
+        const {results} = this.props; 
+        // const time = results.loggedAt; 
+        // const date = time.split("T");
+        // console.log(date);
         return (
             <Fragment>
                 <ul className="list">
                 {console.log(results)}
-                    {results.map(item =>{
+                    {results.map(item => {
                         return(
-                            <li className="list__item">
+                            <li className="list__item" key={item._id} >
                                 <div className="list__item--container">
-                                  <p className="date">{}</p>
+                                  <div className="date"><p className="date">{item.loggedAt.split("T")[0]}</p> <p className="hour">{item.loggedAt.split("T")[1].split("", 8)}</p></div>
                                   <p className="askfor">{item.personRequested}</p>
                                   <p className="name">{item.name}</p>
                                   <p className="company">{item.company}</p>
