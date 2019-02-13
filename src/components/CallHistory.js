230ab23e973d1624c2aa9b44bb7339a0a31027bd
Alex Garcia <alejandragviana@gmail.com>
2019-02-13 16:11:03 +0100

    list printed and grid of the titles done!

--- src/components/CallHistory.js
+++ src/components/CallHistory.js
@@ -8,41 +8,43 @@ class CallHistory extends Component {
         const {results} = this.props; 
         return (
             <Fragment>
-                <div className="main__title">
-                    <div className="main__title--date"><h2>Fecha/hora</h2></div>
-                    <div className="main__title--personRequested"><h2>Preguntaron por</h2></div>
-                    <div className="main__title--whoCalls"><h2>Llamó</h2></div>
-                </div>
-                <div className="main__subtitle">
-                    <div className="main__subtitle--date"><h4>87/87/87</h4></div>
-                    <div className="main__sibtitle--name"><h4>Nombre</h4></div>
-                    <div className="main__subtitle--company"><h4>Empresa</h4></div>
-                    <div className="main__subtitle--other"><h4>Cargo / Detalles / Contacto</h4></div>
-                </div>
-                <ul className="list">
-                    {results.map(item => {
-                        return(
-                            <li className="list__item" key={item._id} >
-                                <div className="list__item--container">
-                                    <div className="date">
-                                        <p className="date">{item.loggedAt.split("T")[0].split("-").reverse().join("/")}</p> 
-                                        <p className="hour">{item.loggedAt.split("T")[1].split("", 5)}h</p>
+                <div className="wrapper__callHistory">
+                    <div className="main__title">
+                        <div className="main__title--date"><h2>Fecha/hora</h2></div>
+                        <div className="main__title--personRequested"><h2>Preguntaron por</h2></div>
+                        <div className="main__title--whoCalls"><h2>Llamó</h2></div>
+                        <div className="main__title--message"><h2>Mensaje</h2></div>
+                    </div>
+                    <div className="main__subtitle">
+                        <div className="main__subtitle--date"><h4>87/87/87</h4></div>
+                        <div className="main__sibtitle--name"><h4>Nombre</h4></div>
+                        <div className="main__subtitle--company"><h4>Empresa</h4></div>
+                        <div className="main__subtitle--other"><h4>Cargo / Detalles / Contacto</h4></div>
+                    </div>
+                    <ul className="list">
+                        {results.map(item => {
+                            return(
+                                <li className="list__item" key={item._id} >
+                                    <div className="list__item--container">
+                                        <div className="date">
+                                            <p className="date__day">{item.loggedAt.split("T")[0].split("-").reverse().join("/")}</p> 
+                                            <p className="hour">{item.loggedAt.split("T")[1].split("", 5)}h</p>
+                                        </div>
+                                        <p className="askfor">{item.personRequested}</p>
+                                        <p className="name">{item.name}</p>
+                                        <p className="company">{item.company}</p>
+                                        <p className="otherinfo">{item.position}{item.otherInfo}{item.email}{item.telephone}</p>
+                                        <div className="messageAndAction">
+                                            <p className="message">{item.message}</p>  
+                                            <p className="action">{item.action}</p>  
+                                        </div>
                                     </div>
-                                    <p className="askfor">{item.personRequested}</p>
-                                    <p className="name">{item.name}</p>
-                                    <p className="company">{item.company}</p>
-                                    <p className="otherinfo">{item.position}{item.otherInfo}{item.email}{item.telephone}</p>
-                                    <div className="messageAndAction">
-                                        <p className="message">{item.message}</p>  
-                                        <p className="action">{item.action}</p>  
-                                    </div>
-                                </div>
-                            </li>
-                        );
-                    })}
-                
-                </ul>
-                
+                                </li>
+                            );
+                        })}
+                    
+                    </ul>
+                </div>
             </Fragment>
         );
     }
