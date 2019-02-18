c4058569b4f95d628fa65a091164a2c8580b8b78
Carlos Mañas <carlos@spacenomads.com>
2019-02-18 12:23:57 +0100

    Add docs/ folder

---
+++ docs/precache-manifest.b523b77ba790f5218366998d872d0b0d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd8bfaed7b844658a2ae2b526a0c1c34",
+    "url": "./static/media/tick.cd8bfaed.svg"
+  },
+  {
+    "revision": "d83cfb6074d02d95bc0cc4bc42e1806c",
+    "url": "./static/media/plus_white.d83cfb60.svg"
+  },
+  {
+    "revision": "92fc80896529f4be57cd6462fdb323c1",
+    "url": "./static/media/plus.92fc8089.svg"
+  },
+  {
+    "revision": "e60bb3ffc4f77c1cad66e89218cd51e3",
+    "url": "./static/media/logo_your_calls.e60bb3ff.svg"
+  },
+  {
+    "revision": "e893630a68ec109ccb885c4cc5799856",
+    "url": "./static/media/logo-interacso-white.e893630a.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1a76cfc02c4139a4fd8d",
+    "url": "./static/js/main.1a76cfc0.chunk.js"
+  },
+  {
+    "revision": "1be87877f9ffab322da3",
+    "url": "./static/js/1.1be87877.chunk.js"
+  },
+  {
+    "revision": "1a76cfc02c4139a4fd8d",
+    "url": "./static/css/main.8dac77cd.chunk.css"
+  },
+  {
+    "revision": "c43cc667b860546ce2f09cfc394b8e43",
+    "url": "./index.html"
+  }
+];
