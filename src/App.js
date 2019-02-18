cc6484a998bef1c8844ab3ad35c8742217342568
Alex Garcia <alejandragviana@gmail.com>
2019-02-18 09:06:20 +0100

    first format for message

--- src/App.js
+++ src/App.js
@@ -49,8 +49,7 @@ class App extends Component {
       endDate: ""
 
   };
-//   console.log('paco');
-// console.log(process.env.REACT_APP_PACO);
+
 
     this.getWhoCalls = this.getWhoCalls.bind(this);
     this.getRequestedEmployee = this.getRequestedEmployee.bind(this);
@@ -254,15 +253,15 @@ getCallAction(event) {
 
   makeMessage(){
 
-    let message=`${this.state.info.personRequested}, te acaban de llamar y te han dejado el *siguiente mensaje*: \n${this.state.info.action} \n${this.state.info.message}`;
+    let message=`*${this.state.info.personRequested}*, te acaban de llamar y te han dejado el siguiente mensaje: \n*${this.state.info.action} \n${this.state.info.message}*`;
 
     if ((this.state.info.name!==''|| this.state.info.position!=='' || this.state.info.company!=='' || this.state.info.otherInfo!==''|| this.state.info.email!=='' )&& this.state.info.telephone===0){
-      return message=  `${this.state.info.personRequested}, *te acaba de llamar*: \n
-      ${this.state.info.name} \n${this.state.info.position} \n${this.state.info.company}  \n${this.state.info.email} \n${this.state.info.otherInfo} \n *Su mensaje ha sido* \n${this.state.info.action} \n${this.state.info.message}`;
+      return message=  `*${this.state.info.personRequested}*, te acaba de llamar: \n
+      *${this.state.info.name} \n${this.state.info.position} \n${this.state.info.company}  \n${this.state.info.email} \n${this.state.info.otherInfo}* \n Su mensaje ha sido \n *${this.state.info.action} \n${this.state.info.message}*`;
     }
       else if (this.state.info.name!==''|| this.state.info.position!=='' || this.state.info.company!=='' || this.state.info.otherInfo!==''|| this.state.info.email!=='' || this.state.info.telephone!==0){
-      return message=  `${this.state.info.personRequested}, *te acaba de llamar*: \n
-      ${this.state.info.name} \n${this.state.info.position} \n${this.state.info.company} \n${this.state.info.telephone} \n${this.state.info.email} \n${this.state.info.otherInfo} \n *Su mensaje ha sido* \n${this.state.info.action} \n${this.state.info.message}`;
+      return message=  `*${this.state.info.personRequested}*, te acaba de llamar: \n
+      *${this.state.info.name} \n${this.state.info.position} \n${this.state.info.company} \n${this.state.info.telephone} \n${this.state.info.email} \n${this.state.info.otherInfo}* \n Su mensaje ha sido \n*${this.state.info.action} \n${this.state.info.message}*`;
 
     }else{
 
